a6deaf274f063a961287cf427cf0b5a8df0f46ce
Taavi Meinberg <taavi.meinberg@gmail.com>
2024-07-02 23:38:48 +0300

    Added dockerfile

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM httpd:2.4
+
+COPY . /usr/local/apache2/htdocs/
+
+RUN mv /usr/local/apache2/htdocs/data-collection /opt/
+
+RUN  apt-get update \
+  && apt-get install -y wget \
+  && rm -rf /var/lib/apt/lists/*
+
+RUN chmod u+x /opt/data-collection/collectData.sh
+
+RUN /opt/data-collection/collectData.sh /usr/local/apache2/htdocs
+
+#TODO add systemd service/timer stuff for updating data
+#TODO refactor collectData.sh to be more location agnostic
--- data-collection/collectData.sh
+++ data-collection/collectData.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+OUTPUT_DIR=$1
+
 #Collect environment data
-wget -O /var/www/html/stats/EE/environmentData.json https://api.stats.x-road.global/v1/instances/EE
-wget -O /var/www/html/stats/ee-test/environmentData.json https://api.stats.x-road.global/v1/instances/ee-test
-wget -O /var/www/html/stats/ee-dev/environmentData.json https://api.stats.x-road.global/v1/instances/ee-dev
+wget -O $OUTPUT_DIR/EE/environmentData.json https://api.stats.x-road.global/v1/instances/EE
+wget -O $OUTPUT_DIR/ee-test/environmentData.json https://api.stats.x-road.global/v1/instances/ee-test
+wget -O $OUTPUT_DIR/ee-dev/environmentData.json https://api.stats.x-road.global/v1/instances/ee-dev
 
 #Collect environment history for timeline
-wget -O /var/www/html/stats/EE/history.json https://api.stats.x-road.global/v1/instances/EE/history
-wget -O /var/www/html/stats/ee-test/history.json https://api.stats.x-road.global/v1/instances/ee-test/history
-wget -O /var/www/html/stats/ee-dev/history.json https://api.stats.x-road.global/v1/instances/ee-dev/history
+wget -O $OUTPUT_DIR/EE/history.json https://api.stats.x-road.global/v1/instances/EE/history
+wget -O $OUTPUT_DIR/ee-test/history.json https://api.stats.x-road.global/v1/instances/ee-test/history
+wget -O $OUTPUT_DIR/ee-dev/history.json https://api.stats.x-road.global/v1/instances/ee-dev/history
