bf02e1f16f9c9286f12d2bce9ef45bb890f5ea7b
Taavi Meinberg <taavi.meinberg@gmail.com>
2024-07-15 23:27:27 +0300

    Changed directory structure

---
+++ data-collection/dataCollection.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+OUTPUT_DIR=$1
+
+if [ -z "$1" ]
+  then
+    echo "Please provide output dir"
+    exit 1
+fi
+
+#Collect environment data
+wget -O $OUTPUT_DIR/EE/environmentData.json https://api.stats.x-road.global/v1/instances/EE
+wget -O $OUTPUT_DIR/ee-test/environmentData.json https://api.stats.x-road.global/v1/instances/ee-test
+wget -O $OUTPUT_DIR/ee-dev/environmentData.json https://api.stats.x-road.global/v1/instances/ee-dev
+
+#Collect environment history for timeline
+wget -O $OUTPUT_DIR/EE/history.json https://api.stats.x-road.global/v1/instances/EE/history
+wget -O $OUTPUT_DIR/ee-test/history.json https://api.stats.x-road.global/v1/instances/ee-test/history
+wget -O $OUTPUT_DIR/ee-dev/history.json https://api.stats.x-road.global/v1/instances/ee-dev/history
